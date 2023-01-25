3b73fc254ea1b1399bc91c4347556bb77cbfe21b
Orange0Range <102272767+Orange0Range@users.noreply.github.com>
2023-01-25 16:36:00 -0600

    23.2 Part 2
    Not testing added yet

--- app.py
+++ app.py
@@ -2,7 +2,8 @@
 
 from flask import Flask, session, request, render_template, jsonify, flash, redirect
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User
+from models import db, connect_db, User, Post
+import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -29,7 +30,9 @@ def show_user(user_id):
     """Show info on a single pet."""
 
     user_info = User.query.get_or_404(user_id)
-    return render_template("user_details.html", user=user_info)
+    made_posts = Post.query.filter(Post.user_id == user_id)
+    has_post = Post.query.filter(Post.user_id == user_id).count()
+    return render_template("user_details.html", user=user_info, posts=made_posts, flag = has_post)
 
 @app.route('/users/<int:user_id>/edit')
 def edit_user_options(user_id):
@@ -57,7 +60,7 @@ def add_user():
     db.session.add(user)
     db.session.commit()
 
-    return redirect(f"/{user.id}")
+    return redirect(f"/users/{user.id}")
 
 @app.route('/users/<int:user_id>/delete')
 def delete_user(user_id):
@@ -65,3 +68,50 @@ def delete_user(user_id):
     user_info = User.query.filter(User.id == user_id).delete()
     db.session.commit()
     return redirect('/')
+
+@app.route('/users/<int:user_id>/posts/new')
+def create_post_form(user_id):
+    """Create Post Form"""
+    user_info = User.query.get_or_404(user_id)
+    return render_template('add_post.html', user = user_info)
+
+@app.route('/users/<int:user_id>/posts/new', methods = ["POST"])
+def create_post(user_id):
+    """Create Post"""
+    title = request.form['title']
+    content = request.form['content']
+    content_a = datetime.datetime.now()
+    new_post = Post(title = title, content = content, content_at = content_a, user_id = user_id)
+    db.session.add(new_post)
+    db.session.commit()
+    return redirect(f"/users/{user_id}")
+
+@app.route('/posts/<int:post_id>')
+def show_post(post_id):
+    """Show Post"""
+    post_info = Post.query.get_or_404(post_id)
+    return render_template("post_details.html", Post=post_info)
+
+@app.route('/posts/<int:post_id>/edit')
+def edit_post_form(post_id):
+    """Edit Post Form"""
+    post_info = Post.query.get_or_404(post_id)
+
+    return render_template('edit_post.html', Post=post_info)
+
+@app.route('/posts/<int:post_id>/edit', methods = ["POST"])
+def edit_post(post_id):
+    """Edit Post"""
+    post_info = Post.query.get_or_404(post_id)
+    post_info.title = request.form['title']
+    post_info.content = request.form['content']
+    post_info.content_at = datetime.datetime.now()
+    db.session.commit()
+    return redirect(f"/posts/{post_id}")
+
+@app.route('/posts/<int:post_id>/delete')
+def delete_post(post_id):
+    """Delete Post"""
+    post_info = Post.query.filter(Post.id == post_id).delete()
+    db.session.commit()
+    return redirect(f"/")
--- models.py
+++ models.py
@@ -16,4 +16,25 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement = True)
     first_name = db.Column(db.String(50), nullable = False, unique = False)
     last_name =  db.Column(db.String(50), nullable = False, unique = False)
-    image_url =  db.Column(db.String(250), nullable = True, unique = False)
+    image_url =  db.Column(db.String(500), nullable = True, unique = False)
+
+    def __repr__(self):
+        e = self
+        return f"<User {e.id} {e.first_name} {e.last_name} {e.image_url}>"
+
+class Post(db.Model):
+    """Post Model"""
+
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.String(50), nullable = False, unique = False)
+    content = db.Column(db.String(1000), nullable = False, unique = False)
+    content_at = db.Column(db.DateTime, nullable = False, unique = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    use = db.relationship('User', backref = 'posts')
+
+    def __repr__(self):
+        e = self
+        return f"<Post {e.id} {e.title} {e.content_at} {e.user_id}>"
